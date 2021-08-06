e4bb74a324ea9e696e4b6516887aa381998461a2
GANYMEDES <vandersar327@gmail.com>
2021-08-06 16:07:59 +0900

    Add Package Structure.

---
+++ app/src/main/java/com/bae/photoproject/utils/Constants.kt
@@ -0,0 +1,10 @@
+package com.bae.photoproject.utils
+
+object Constants
+{
+    const val LOG_TAG: String = "JSLog"
+
+    // API
+    const val BASE_URL = "https://api.pexels.com/v1/"
+    const val API_SEARCH_ENDPOINT = "search?"
+}
--- app/src/main/java/com/bae/photoproject/view/activities/MainActivity.kt
+++ app/src/main/java/com/bae/photoproject/view/activities/MainActivity.kt
@@ -1,9 +1,11 @@
-package com.bae.photoproject
+package com.bae.photoproject.view.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bae.photoproject.R
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+{
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
