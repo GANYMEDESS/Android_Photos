73c013b3ed169d12de11f93cd81f3e27fab3b277
GANYMEDES <vandersar327@gmail.com>
2021-08-09 15:51:01 +0900

    add search & recyclerview in searchfragment.

--- app/src/main/java/com/bae/photoproject/view/fragments/SearchPhotosFragment.kt
+++ app/src/main/java/com/bae/photoproject/view/fragments/SearchPhotosFragment.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.Fragment
 import com.bae.photoproject.databinding.FragmentSearchPhotosBinding
+import com.bae.photoproject.utils.JSLog
 
 class SearchPhotosFragment : Fragment()
 {
@@ -17,6 +19,25 @@ class SearchPhotosFragment : Fragment()
     ): View? {
         mBinding = FragmentSearchPhotosBinding.inflate(inflater, container, false)
         return mBinding?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        mBinding?.svPhotos?.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            android.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                JSLog.i("TextSubmit Search -> $query")
+
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                JSLog.i("onQueryTextChange $newText")
+
+                return true
+            }
+        })
     }
 
     override fun onDestroy() {
